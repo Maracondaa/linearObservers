12ef68e207bd0a54a2365bca13c0fb24081d4ff3
Maracondaa <anna.len.mueller@gmail.com>
2024-01-11 00:34:27 +0100

    roc calculation

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ from createImages import generate_synthetic_image
 from linearObservers import region_of_interest_observer, non_pre_whitening_observer, region_of_interest_observer_ske, \
     pre_whitening_observer, hotelling_observer
 
+# Variables for image generation:
+image_size = (64, 64)
+target_position = (32, 32)
+target_amplitude = target_mean = -1.5
+target_radius = 8.0
+noise_std = 0.1
 
-def main():
-    # Variables for image generation:
-    image_size = (64, 64)
-    target_position = (32, 32)
-    target_amplitude = target_mean = -1.5
-    target_radius = 8.0
-    noise_std = 0.1
 
+def generate_discriminants_for_two_images():
     # Generate an image with a random background, target signal, and noise
     image_with_target = generate_synthetic_image(image_size, target_position, target_amplitude, target_radius,
                                                  noise_std)
@@ -70,7 +70,83 @@ def main():
 
     print('HO Observer (signal present): ', ho_response_with_target)
     print('HO Observer (signal absent): ', ho_response_without_target)
+    return
 
+
+def generate_roc_for_threshold(threshold):
+    roi_ske_true_positives = 0
+    roi_ske_false_positives = 0
+    roi_true_positives = 0
+    roi_false_positives = 0
+    npw_true_positives = 0
+    npw_false_positives = 0
+    pw_true_positives = 0
+    pw_false_positives = 0
+    ho_true_positives = 0
+    ho_false_positives = 0
+    for i in range(0, 100):
+        # Generate an image with a random background, target signal, and noise
+        image_with_target = generate_synthetic_image(image_size, target_position, target_amplitude, target_radius,
+                                                     noise_std)
+        # Generate an image without a target signal but with a random background and noise
+        image_without_target = generate_synthetic_image(image_size, noise_std=noise_std)
+
+        # Calculation of the linear discriminant for the ROI observer (SKE) for both images
+        roi_ske_response_with_target = region_of_interest_observer_ske(image_with_target, target_mean, target_position,
+                                                                       target_radius)
+        roi_ske_response_without_target = region_of_interest_observer_ske(image_without_target, target_mean,
+                                                                          target_position, target_radius)
+        # Calculation of the linear discriminant for the ROI observer for both images
+        roi_response_with_target = region_of_interest_observer(image_with_target, target_mean)
+        roi_response_without_target = region_of_interest_observer(image_without_target, target_mean)
+
+        # Calculation of the linear discriminant for the NPW observer for both images
+        npw_response_with_target = non_pre_whitening_observer(image_with_target, target_mean, 0, 0)
+        npw_response_without_target = non_pre_whitening_observer(image_without_target, target_mean, 0, 0)
+
+        # Calculation of the linear discriminant for the PW observer for both images
+        pw_response_with_target = pre_whitening_observer(image_with_target, target_mean, 0, 1, 0, noise_std)
+        pw_response_without_target = pre_whitening_observer(image_without_target, target_mean, 0, 1, 0, noise_std)
+
+        # Calculation of the linear discriminant for the Hotelling observer for both images
+        ho_response_with_target = hotelling_observer(image_with_target, target_mean, 0, 0, 1, 0,
+                                                     noise_std, 0.5, 0.5)
+        ho_response_without_target = hotelling_observer(image_without_target, target_mean, 0, 0, 1, 0,
+                                                        noise_std, 0.5, 0.5)
+
+        # Determine number of true positives and false positives
+        if roi_ske_response_with_target > threshold: roi_ske_true_positives += 1
+        if roi_ske_response_without_target > threshold: roi_ske_false_positives += 1
+
+        if roi_response_with_target > threshold: roi_true_positives += 1
+        if roi_response_without_target > threshold: roi_false_positives += 1
+
+        if npw_response_with_target > threshold: npw_true_positives += 1
+        if npw_response_without_target > threshold: npw_false_positives += 1
+
+        if pw_response_with_target > threshold: pw_true_positives += 1
+        if pw_response_without_target > threshold: pw_false_positives += 1
+
+        if ho_response_with_target > threshold: ho_true_positives += 1
+        if ho_response_without_target > threshold: ho_false_positives += 1
+
+    print("ROC for threshold T = ", threshold)
+
+    print("ROI-Observer (SKE): ", roi_ske_true_positives / 100, " (TPR), ", roi_ske_false_positives / 100, " (FPR)")
+
+    print("ROI-Observer: ", roi_true_positives / 100, " (TPR), ", roi_false_positives / 100, " (FPR)")
+
+    print("NPW-Observer (SKE): ", npw_true_positives / 100, " (TPR), ", npw_false_positives / 100, " (FPR)")
+
+    print("PW-Observer (SKE): ", pw_true_positives / 100, " (TPR), ", pw_false_positives / 100, " (FPR)")
+
+    print("HO-Observer (SKE): ", ho_true_positives / 100, " (TPR), ", ho_false_positives / 100, " (FPR)")
+    return
+
+
+def main():
+    generate_discriminants_for_two_images()
+    #generate_roc_for_threshold(4)
     return
 
 
